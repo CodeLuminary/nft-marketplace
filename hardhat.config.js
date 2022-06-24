63c1a6daa87e30bd0b749440e10cfae0f9f42629
CodeLuminary <victorijoni@yahoo.com>
2022-06-24 15:08:10 +0100

    hardhat configuration file refactored

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    artifacts: "./artifacts",
+    sources: "./contracts",
+    cache: "./cache",
+    tests: "./test"
+  },
 };
