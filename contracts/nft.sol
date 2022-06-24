c73b3e900d4f9fc8392e63c57ced1a2105ad10ed
CodeLuminary <victorijoni@yahoo.com>
2022-06-24 12:48:54 +0100

    Smart contract refactored

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract Nft is ERC721URIStorage, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
